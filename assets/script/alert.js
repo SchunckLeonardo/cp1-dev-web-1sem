3d9f689f27436b44503ab29d69d5223aad38e9de
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2023-04-28 20:28:02 -0300

    Adicionando o alert

---
+++ assets/script/alert.js
@@ -0,0 +1,7 @@
+const tituloPagina = document.title
+
+if(tituloPagina == "Vinhacity - Home") {
+    alert("Seja bem vindo")
+} else if(tituloPagina == "Vinhacity - Quiz") {
+    alert("Vamos conhecer sobre vinho")
+}
